b4c1130646a57339f22a6049a3579194ee7abca1
Asikur Rahman <asikurbd25@gmail.com>
2024-09-29 12:18:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function callAll(jsfiles) {
     src.setAttribute("src", jsfiles);
     document.getElementsByTagName("head")[0].appendChild(src);
 }
-callAll("./js/bijoy2uni.js");
-callAll("./js/uni2bijoy.js");
-callAll("./js/common.js");
-callAll("./js/layout.js");
-callAll("./js/count.js");
-callAll("./js/js1.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/bijoy2uni.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/uni2bijoy.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/common.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/layout.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/count.js");
+callAll("https://cdn.jsdelivr.net/gh/Asikurbd/font-converter/js1.js");
